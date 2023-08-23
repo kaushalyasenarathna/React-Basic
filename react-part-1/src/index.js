dff401fe3633f3b1b2d78b21318ba6e3b15cfb9f
Your Name <you@example.com>
2023-08-23 18:10:13 +0530

    second commit

--- react-part-1/src/index.js
+++ react-part-1/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+ 
 
+const Header=(props)=>  {
+  const {title ,subtitle} = props
+  return(
+    <div>
+    <h1>{title}</h1>
+    <h2>{subtitle}</h2>
+
+    </div>
+  )
+}
 
 const App=()=>{
   return(
     <div>
-      <h1>Hello React App</h1>
+      <Header title="Hello World" subtitle="Hey React App"/>
     </div>
   )
 }
